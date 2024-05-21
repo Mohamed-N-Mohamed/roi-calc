d5878d0dc60085e2d7b9d6d52a278384882922bf
Mohamed Mohamed <mohamed.mohamed@bennamann.com>
2024-05-21 15:21:24 +0100

    Add Farmer Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import FarmerInfo from './components/FarmerInfo';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className='main'>
-      <p>ROI</p>
+      <Container maxWidth='md'>
+        <CssBaseline />
+        <FarmerInfo />
+      </Container>
     </div>
   );
 }
---
+++ src/components/FarmerInfo.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const farmTypes = [
+  {
+    values: 'dairy farms',
+    label: 'dairy farms',
+  },
+
+  {
+    values: 'dairy farms 2',
+    label: 'dairy farms 2 ',
+  },
+
+  {
+    values: 'dairy farms 3',
+    label: 'dairy farms 3 ',
+  },
+];
+
+const FarmerInfo = () => {
+  return (
+    <Box
+      sx={{
+        marginTop: '20px',
+        width: '100%',
+        height: '80vh',
+        backgroundColor: '#e8edf0',
+        borderRadius: '5px',
+      }}
+    >
+      <Typography
+        component='h1'
+        variant='h5'
+        textAlign='center'
+        sx={{ paddingTop: '1rem' }}
+      >
+        Farmer Information
+      </Typography>
+
+      <Box
+        component='form'
+        sx={{
+          padding: '2rem',
+          fontSize: '28px',
+        }}
+      >
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            gap: '1rem',
+            alignItems: 'center',
+          }}
+        >
+          <TextField
+            margin='normal'
+            required
+            label='Full Name'
+            sx={{ backgroundColor: '#fff', width: '50%' }}
+          />
+          <TextField
+            margin='normal'
+            required
+            label='Address '
+            sx={{ backgroundColor: '#fff', width: '50%' }}
+          />
+        </Box>
+        <TextField
+          margin='normal'
+          required
+          label='Email'
+          fullWidth
+          sx={{ backgroundColor: '#fff' }}
+        />
+
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            gap: '1rem',
+            alignItems: 'center',
+            justifyContent: 'cen',
+          }}
+        >
+          <TextField
+            margin='normal'
+            required
+            label='Farm Type'
+            select
+            sx={{ width: '30%', backgroundColor: '#fff' }}
+          >
+            {farmTypes.map((farms) => (
+              <MenuItem key={farms.values} value={farms.values}>
+                {farms.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            margin='normal'
+            required
+            label='Phone'
+            fullWidth
+            sx={{ backgroundColor: '#fff', width: '40%' }}
+          />
+          {/* <FormControlLabel control={<Checkbox />} label='Yes, I agree to the requirement that you just layout it' sx={{width: '30%'}}/> */}
+          <Box>
+            <Typography component='h6' variant='h6'>
+              Farmer Information
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default FarmerInfo;
